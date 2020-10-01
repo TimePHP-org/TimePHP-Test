607f4464d359c331a4c2abc44020efae0ea5aa39
MrAnyx <1ptitigre@gmail.com>
2020-10-01 22:37:59 +0200

    [add] Added example

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 
-require __DIR__ . "/vendor/autoload.php";
+// require __DIR__ . "/vendor/autoload.php";
 
-if(class_exists("TimePHP\Foundation\Router")){
-   echo "Router exists";
-} else{
-   echo "Router doesn't exists";
-}
+// if(class_exists("TimePHP\Foundation\Router")){
+//    echo "Router exists";
+// } else{
+//    echo "Router doesn't exists";
+// }
+
+
+$values = [
+   ["a","b","c","d"],
+   ["a","b","c","d"]
+];
+
+$newValues = array_map(function($element) {
+   return json_decode($element);
+}, array_unique(array_map(function($element) {
+   return json_encode($element);
+}, $values)));
+
+var_dump($values, $newValues);
