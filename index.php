b2ea61dd090b22a1850d39c67bcd0aae3e2c94a1
MrAnyx <1ptitigre@gmail.com>
2020-10-16 17:40:03 +0200

    [fix] Fixed File name

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+// shell_exec("composer remove symfony/var-dumper");
+// echo "test";
 
-shell_exec("composer remove symfony/var-dumper");
-echo "test";
+$http = new \TimePHP\UrlParser\Parser();
