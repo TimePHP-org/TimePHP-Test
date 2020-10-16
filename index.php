aade7f1a206d6cb22d5a30b3f319ddbe14ee5cdb
MrAnyx <1ptitigre@gmail.com>
2020-10-16 22:35:21 +0200

    [feat] Added test for urlParser

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+
 require __DIR__ . "/vendor/autoload.php";
 
 // shell_exec("composer remove symfony/var-dumper");
 // echo "test";
 
 $http = new \TimePHP\UrlParser\Parser();
+
+dd($http->get("int"));
