b09f08790da21996d06e25d024462266a0790a29
MrAnyx <1ptitigre@gmail.com>
2020-10-19 16:32:39 +0200

    [feat] keep experimenting urlparser

--- index.php
+++ index.php
@@ -7,5 +7,23 @@ require __DIR__ . "/vendor/autoload.php";
 // echo "test";
 
 $http = new \TimePHP\UrlParser\Parser();
+dump($http->post());
+?>
 
-dd($http->get("int"));
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Test</title>
+</head>
+
+<body>
+   <form method="POST">
+      <input type="text" name="username">
+      <input type="submit">
+   </form>
+</body>
+
+</html>
